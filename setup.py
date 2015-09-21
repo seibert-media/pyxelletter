79530b241bcf3e1571c011911434c4850cd00a07
Nabil Nasri <nnasri@seibert-media.net>
2015-09-21 20:17:55 +0200

    Version Push

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyxelletter',
-    version='0.5.1',
+    version='0.6',
     install_requires=['requests==2.7.0'],
     packages=['pyxelletter'],
     url='https://github.com/seibert-media/pyxelletter',
