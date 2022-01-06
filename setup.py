c76ca10cbd3e4f25ad2eaf9f3761fdf169acd293
Jean Petry <jpetry@seibert-media.net>
2022-01-06 12:31:30 +0100

    Add info to setup

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='pyxelletter',
     version='0.7',
     install_requires=['requests>=2.20.0'],
@@ -10,4 +13,12 @@ setup(
     author='Nabil Nasri, Jean Petry',
     author_email='jpetry@seibert-media.net',
     description='A Python library for http://www.pixelletter.de/',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
 )
